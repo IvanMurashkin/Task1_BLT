404d81b360283e96b6e3c89be41e9b2305007d41
Мурашкин Иван <murashkin1@yandex.ru>
2019-04-29 22:49:38 +0400

    Калькулятор для суммы чисел

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let resultPopup = document.querySelector('.result-popup');
+let btnCalc = document.querySelector('.btn-calc');
+let btnClose = document.querySelector('.btn-close');
+
+btnCalc.addEventListener('click', btnCalcHandler);
+btnClose.addEventListener('click', btnCloseHandler);
+
+//Обработчик кнопки "Ок" в попапе с результатом
+function btnCloseHandler(e) {
+    resultPopup.style.display = 'none';
+}
+
+//Обработчик кнопки "Сложить"
+function btnCalcHandler(e) {
+    e.preventDefault();
+    let firstNum = document.querySelector('#first-number');
+    let secondNum = document.querySelector('#second-number');
+    let result = sum(Number(firstNum.value), Number(secondNum.value));
+    outputReslutInPopup(result)
+}
+
+function sum(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+//Вывод результата сложения в попапе
+function outputReslutInPopup(result) {
+    document.querySelector('.result').innerHTML = result;
+    resultPopup.style.display = 'inline-block';
+}
